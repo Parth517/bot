0bc476f950228261238f966d7b0d405217b24b81
Parth Deshpande <parthjun27@gmail.com>
2021-03-22 15:08:38 +0530

    Kick and ban

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const client = new Discord.Client();
 const config = require('./config.json');
 const command = require('./command');
 const firstMessage = require('./first-message');
-const roleClaims = require('./role-claim');
 const roleClaim = require('./role-claim');
 
 client.on('ready', () =>{
@@ -26,10 +25,52 @@ client.on('ready', () =>{
                 name: `"${prefix}help"for help`
             },
         });
-        roleClaim(client);
+        //roleClaim(client);
     
     command(client,['ping','test'],message =>{
         message.channel.send('Pong!')
+    });
+    command(client,'ban',message =>{
+        const {member,mentions}=message
+        const tag = `<@${member.id}>`
+
+        if(
+            member.hasPermission('ADMINISTRATOR') || 
+            member.hasPermission('BAN_MEMBERS')
+            )
+        {
+            const target = mentions.users.first()
+            if (target){
+                const targetMember = message.guild.members.cache.get(target.id)
+                targetMember.ban()
+                message.channel.send(`${tag} The user has been banned`)
+            }else{
+                message.channel.send(`${tag} Please specify someone to ban.`)
+            }
+          }else{
+            message.channel.send(`${tag} You do not have the permission to use this command`)
+        }
+    });
+    command(client,'kick',message =>{
+        const {member,mentions}=message
+        const tag = `<@${member.id}>`
+
+        if(
+            member.hasPermission('ADMINISTRATOR') || 
+            member.hasPermission('KICK_MEMBERS')
+            )
+        {
+            const target = mentions.users.first()
+            if (target){
+                const targetMember = message.guild.members.cache.get(target.id)
+                targetMember.kick()
+                message.channel.send(`${tag} The user has been kicked`)
+            }else{
+                message.channel.send(`${tag} Please specify someone to kick.`)
+            }
+          }else{
+            message.channel.send(`${tag} You do not have the permission to use this command`)
+        }
     });
     command(client, 'servers',message =>{
         client.guilds.cache.forEach((guild)=>{
